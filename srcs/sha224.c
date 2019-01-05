318de63b450e492fcba4e179490c687dd33b1a3c
Basile Durst <bdurst2812@gmail.com>
2019-01-05 13:20:21 +0100

    norme 7

--- srcs/sha224.c
+++ srcs/sha224.c
@@ -6,14 +6,14 @@
 /*   By: bdurst2812 <bdurst2812@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/02 12:11:55 by bdurst2812        #+#    #+#             */
-/*   Updated: 2019/01/05 13:14:39 by bdurst2812       ###   ########.fr       */
+/*   Updated: 2019/01/05 13:20:07 by bdurst2812       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
 
-static uint32_t g_k[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
-		0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 
+static uint32_t g_k[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+		0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
 		0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
 		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa,
 		0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
@@ -120,7 +120,7 @@ char			*sha224(char *message)
 	uint32_t	padding_message_len;
 	uint8_t		*padding_message;
 	uint32_t	offset;
-	char 		*encode_message;
+	char		*encode_message;
 
 	data.h[0] = 0xc1059ed8;
 	data.h[1] = 0x367cd507;
